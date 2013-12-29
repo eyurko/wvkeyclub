0e9a6c21bdedd9a8041802fac6bcd0de6078965f
Eric Yurko <eric.yurko@gmail.com>
2013-12-28 16:29:34 -0800

    Updated site
    Updated site for 13-14 KC Year (bit late, I know)

--- New Key Club Site/js/FeaturedjCapSlide.js
+++ New Key Club Site/js/FeaturedjCapSlide.js
@@ -48,4 +48,74 @@ $(function() {
                     border			: '5px solid #52969E',
                     showcaption	    : true
                 });
+				$("#capslide_img_cont8").capslide({
+                    caption_color	: 'black',
+                    caption_bgcolor	: 'white',
+                    overlay_bgcolor : 'white',
+                    border			: '5px solid #52969E',
+                    showcaption	    : true
+                });
+                $("#capslide_img_cont9").capslide({
+                    caption_color	: 'black',
+                    caption_bgcolor	: 'white',
+                    overlay_bgcolor : 'white',
+                    border			: '5px solid #52969E',
+                    showcaption	    : true
+                });
+                $("#capslide_img_cont10").capslide({
+                    caption_color	: 'black',
+                    caption_bgcolor	: 'white',
+                    overlay_bgcolor : 'white',
+                    border			: '5px solid #52969E',
+                    showcaption	    : true
+                });
+				$("#capslide_img_cont11").capslide({
+                    caption_color	: 'black',
+                    caption_bgcolor	: 'white',
+                    overlay_bgcolor : 'white',
+                    border			: '5px solid #52969E',
+                    showcaption	    : true
+                });
+                $("#capslide_img_cont12").capslide({
+                    caption_color	: 'black',
+                    caption_bgcolor	: 'white',
+                    overlay_bgcolor : 'white',
+                    border			: '5px solid #52969E',
+                    showcaption	    : true
+                });
+                $("#capslide_img_cont13").capslide({
+                    caption_color	: 'black',
+                    caption_bgcolor	: 'white',
+                    overlay_bgcolor : 'white',
+                    border			: '5px solid #52969E',
+                    showcaption	    : true
+                });
+                $("#capslide_img_cont14").capslide({
+                    caption_color	: 'black',
+                    caption_bgcolor	: 'white',
+                    overlay_bgcolor : 'white',
+                    border			: '5px solid #52969E',
+                    showcaption	    : true
+                });
+                $("#capslide_img_cont15").capslide({
+                    caption_color	: 'black',
+                    caption_bgcolor	: 'white',
+                    overlay_bgcolor : 'white',
+                    border			: '5px solid #52969E',
+                    showcaption	    : true
+                });
+                $("#capslide_img_cont16").capslide({
+                    caption_color	: 'black',
+                    caption_bgcolor	: 'white',
+                    overlay_bgcolor : 'white',
+                    border			: '5px solid #52969E',
+                    showcaption	    : true
+                });
+                $("#capslide_img_cont17").capslide({
+                    caption_color	: 'black',
+                    caption_bgcolor	: 'white',
+                    overlay_bgcolor : 'white',
+                    border			: '5px solid #52969E',
+                    showcaption	    : true
+                });
             });
